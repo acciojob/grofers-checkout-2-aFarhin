f1a2679735446699e9dfd361ec9994874c64b634
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-31 17:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-describe('example to-do app', () => {
-  it('check number of prices', () => {
-    cy.get('[data-ns-test="prices"]').should('have.length', 3);
-  });
+// Get all the prices listed in the HTML table
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
 
-  it('check total prices from table equals grand total', () => {
-    let totalPrice = 0;
-    cy.get('[data-ns-test="prices"]').each(price => {
-      totalPrice += parseFloat(price.text());
-    });
-
-    cy.get('[data-ns-test="grandTotal"]').should('have.text', totalPrice.toFixed(2));
-  });
+// Calculate the sum of all prices
+let totalPrice = 0;
+prices.forEach(price => {
+  totalPrice += parseFloat(price.textContent);
 });
+
+// Create a new row in the HTML table to display the total price
+const table = document.querySelector('table');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+newCell.setAttribute('data-ns-test', 'grandTotal');
+newCell.textContent = totalPrice.toFixed(2);
